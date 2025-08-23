23c3574be7fe7794801cbf9ad28b9603cbabacef
Divya B Gowda <117468949+DivyaBGowda484@users.noreply.github.com>
2025-08-24 00:30:05 +0530

    Initial Commit

---
+++ core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.mdels import User
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roll_no = models.CharField(max_length=20, unique=True)
+    courses = models.ManyToManyField(Course, through="Enrollment")
+
+    def __str__(self):
+        return f'{self.user.username} - self.roll_no'
+    
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    courses = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student} enrolled in {self.course}"
